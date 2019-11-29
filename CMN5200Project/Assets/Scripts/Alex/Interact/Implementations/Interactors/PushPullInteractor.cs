de9e3742884f5ee1aeb4ae026fab3bb329f81666
AnemogiannisAlexandros <alex.anemogiannis@hotmail.com>
2019-11-29 21:38:10 +0200

    Slight Changes

--- CMN5200Project/Assets/Scripts/Alex/Interact/Implementations/Interactors/PushPullInteractor.cs
+++ CMN5200Project/Assets/Scripts/Alex/Interact/Implementations/Interactors/PushPullInteractor.cs
@@ -6,26 +6,33 @@ public class PushPullInteractor : IInteractor
 {
     [SerializeField] private LayerMask m_Interactable;
     [SerializeField] private float raycastDistance;
-    private RaycastHit2D raycastHit;
+    private Collider2D raycastHit;
+    private BoxCollider2D interactionCollider;
+    bool foundObject;
 
+    private void Awake()
+    {
+        interactionCollider = GetComponent<BoxCollider2D>();
+    }
     public override void InteractCondition()
     {
         allowMovement = true;
         //raycastHit = Physics2D.Raycast(transform.position, new Vector2(transform.localScale.x,0), raycastDistance, m_Interactable);
         //Debug.DrawRay(transform.position, new Vector2(transform.localScale.x, 0) * raycastDistance, Color.green);
-        raycastHit = Physics2D.BoxCast(transform.position + new Vector3(transform.localScale.x*1.5f,-.2f,0), transform.localScale * raycastDistance, 0, new Vector2(transform.localScale.x, transform.position.y),raycastDistance,m_Interactable);
-        Debug.Log(canInteract);
+        //raycastHit = Physics2D.BoxCast(transform.position + new Vector3(transform.localScale.x*1.5f,-.2f,0), transform.localScale * raycastDistance,0,new Vector2(transform.position.x * transform.localScale.x,transform.position.y),raycastDistance,m_Interactable);
+        //Debug.Log(canInteract);
+        //Debug.Log(raycastHit.point);
 
-        if (raycastHit.collider != null)
+        //if (raycastHit.collider != null)
+        //{
+        //    if (raycastHit.collider.gameObject != gameObject)
+        //    {
+        //        if (raycastHit.distance <= raycastDistance)
+        //        {
+        if (foundObject)
         {
-            if (raycastHit.collider.gameObject != gameObject)
-            {
-                if (raycastHit.distance <= raycastDistance)
-                {
-                    Debug.Log("Hittin Collider");
-                    canInteract = true;
-                }
-            }
+            Debug.Log("Hittin Collider");
+            canInteract = true;
         }
         else
         {
@@ -35,15 +42,39 @@ public class PushPullInteractor : IInteractor
     public override void Interact()
     {
         base.Interact();
-        Debug.Log("Hit : " + raycastHit.collider.name);
-        GameObject obj = raycastHit.collider.gameObject;
+        Debug.Log("Hit : " + raycastHit.name);
+        GameObject obj = raycastHit.gameObject;
         float xDistance = GetComponent<CapsuleCollider2D>().size.x / 2 + obj.transform.lossyScale.x / 2;
         obj.transform.position = Vector3.MoveTowards(obj.transform.position, (transform.position + transform.right * xDistance / 2 * transform.localScale.x * 1.3f), 0.1f);
         // = transform.position + transform.right * xDistance/2 * transform.localScale.x *1.3f;
         obj.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
     }
-    private void OnDrawGizmos()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        Gizmos.DrawCube(transform.position + new Vector3(transform.localScale.x*1.5f,-.2f, 0), transform.localScale * raycastDistance);
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Interactable")) 
+        {
+            Debug.Log(collision.name);
+            raycastHit = collision;
+            foundObject = true;
+        }
     }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Interactable"))
+        {
+            raycastHit = collision;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Interactable"))
+        {
+            Debug.Log(collision.name);
+            foundObject = false;
+        }
+    }
+    //private void OnDrawGizmos()
+    //{
+    //    Gizmos.DrawCube(transform.position + new Vector3(transform.localScale.x*1.5f,-.2f, 0), transform.localScale * raycastDistance);
+    //}
 }
