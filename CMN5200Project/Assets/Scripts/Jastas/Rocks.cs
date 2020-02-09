b93d973aaa6feeab2ad5ed4bf9769c1c9d057007
alexandros <alex.anemogiannis@hotmail.com>
2020-02-09 21:11:40 +0200

    Latest merge

---
+++ CMN5200Project/Assets/Scripts/Jastas/Rocks.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace Jastas {
+    
+    public class Rocks : MonoBehaviour {
+
+        [SerializeField] private Rigidbody2D[] rocks;
+
+        private void Start() {
+            for (int i = 0; i < rocks.Length; i++) {
+                rocks[i] = GameObject.FindGameObjectsWithTag("Rocks")[i].GetComponent<Rigidbody2D>();
+                rocks[i].bodyType = RigidbodyType2D.Kinematic;
+            }
+        }
+
+        private void OnTriggerEnter2D(Collider2D other) {
+            for (int i = 0; i < rocks.Length; i++) {
+                if (other.gameObject.CompareTag("Player")) {
+                    rocks[i].bodyType = RigidbodyType2D.Dynamic;
+                    rocks[i].mass = 1000;
+                    GameManager.Instance.Player.Damage();
+                    Debug.Log("player damage");
+                }
+            }
+        }
+    }
+}
